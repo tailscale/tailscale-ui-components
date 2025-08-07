7b88b996619486a4c2019735187305e92e15a58c
Paul Rubritz <paulr@tailscale.com>
2025-08-07 16:35:43 -0400

    export fix again

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 import svgr from "vite-plugin-svgr"
 import paths from "vite-tsconfig-paths"
-
 
 export default defineConfig({
   plugins: [paths(), react(), svgr()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'TailscaleUiComponents',
-      fileName: 'tailscale-ui-components',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "TailscaleUiComponents",
+      formats: ["es", "umd"],
+      fileName: (format) => `tailscale-ui-components.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'tailwindcss'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          tailwindcss: 'tailwindcss',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
