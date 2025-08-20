8951a21551c7c4c156fdb51931eda5cde413db3b
Paul Rubritz <paulr@tailscale.com>
2025-08-19 22:30:33 -0400

    added Storybook Designs support, added first Figma file link

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,7 +11,8 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
+    "@storybook/addon-designs",
   ],
   staticDirs: ['../src/assets'],
   "framework": {
